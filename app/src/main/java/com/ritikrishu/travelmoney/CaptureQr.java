0157ce5f7e8b924be88dbda26c215d9f7b95622e
sanyamtyagi <sanyam.tyagi@hotcocoasoftware.com>
2016-06-09 20:27:53 +0530

    Merge branch 'master' of /Users/sanyamtyagi/Documents/projects/TravelMoney with conflicts.

---
+++ app/src/main/java/com/ritikrishu/travelmoney/CaptureQr.java
@@ -0,0 +1,12 @@
+package com.ritikrishu.travelmoney;
+
+import com.journeyapps.barcodescanner.CameraPreview;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by sanyamtyagi on 09/06/16.
+ */
+public class CaptureQr extends CaptureActivity {
+
+
+}
